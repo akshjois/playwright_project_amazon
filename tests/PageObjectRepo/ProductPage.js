57bf5024948fd33368a67a12c40815b6de83fa99
akshjois <akshatha6557@gmail.com>
2025-10-20 16:49:56 +0530

    updated cart locator

--- tests/PageObjectRepo/ProductPage.js
+++ tests/PageObjectRepo/ProductPage.js
@@ -3,7 +3,7 @@ class ProductPage{
         this.page = page;
         this.addTocart = page.getByRole("button", { name: "Add to Cart" });
         this.confirmMessage = page.locator('h1:has-text("Added to Cart")');
-        this.goToCartbtn = page.getByRole('link', { name: 'Go to Cart' });
+        this.goToCartbtn = page.getByRole('link', { name: 'Go to Cart' }).nth(1);
     }
 
     async addToCart(page) {
