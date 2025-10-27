a9fefd2ce43535576a9568fa9a506bea741e97d7
akshjois <akshatha6557@gmail.com>
2025-10-27 21:05:48 +0530

    pipeline new file created

---
+++ playwright.service.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require('@playwright/test');
+const { createAzurePlaywrightConfig, ServiceOS } = require('@azure/playwright');
+const { DefaultAzureCredential } = require('@azure/identity');
+const config = require('./playwright.config');
+
+/* Learn more about service configuration at https://aka.ms/pww/docs/config */
+export default defineConfig(
+  config,
+  createAzurePlaywrightConfig(config, {
+    exposeNetwork: '<loopback>',
+    connectTimeout: 3 * 60 * 1000, // 3 minutes
+    os: ServiceOS.LINUX,
+    credential: new DefaultAzureCredential(),
+  }),
+   {
+    /* 
+    Playwright Testing service reporter is added by default.
+    This will override any reporter options specified in the base playwright config.
+    If you are using more reporters, please update your configuration accordingly.
+    */
+    reporter: [['list'], ['@azure/microsoft-playwright-testing/reporter']],
+  }
+);
